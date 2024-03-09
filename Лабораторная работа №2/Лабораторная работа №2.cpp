1c7ac56231c2a994c0db08bb5aca29d1e97d9ca7
MaxMen5 <161643770+MaxMen5@users.noreply.github.com>
2024-03-09 23:33:18 +0300

    Добавьте файлы проекта.

---
+++ Лабораторная работа №2/Лабораторная работа №2.cpp
@@ -0,0 +1,163 @@
+﻿#include <iostream>
+using namespace std;
+
+void instruction() {
+	cout << "Для добавления элемента в очередь, введите 1\n";
+	cout << "Для извлечения элемента из очереди, введите 2\n";
+	cout << "Для подсчета количества элементов в очереди, введите 3\n";
+	cout << "Для очистки всей очереди, введите 4\n";
+	cout << "Для подсчета количества конкретного элемента в очереди, введите 5\n";
+	cout << "Для удаления отрицательных элементов из очереди, введите 6\n";
+	cout << "Для вставки перед каждым отрицательным элементом числа 1, введите 7\n";
+	cout << "Для завершения работы программы, введите 8\n\n";
+}
+
+struct Node {
+	int param = 0;
+	Node* next = nullptr;
+};
+
+struct Queue {
+
+	int counting = 0;
+	Node* last = nullptr;
+	Node* first = nullptr;
+
+	int count() {
+		return counting;
+	}
+	void queue(int value) {
+		Node* node = new Node();
+		node->param = value;
+		if (count() == 0) { // При добавлении первого элемента
+			last = node;
+			first = last;
+		}
+		else { // При добавлении остальных элементов
+			last->next = node;
+			last = node;
+		}
+		counting++;
+	}
+	int unqueue() {
+		if (count() == 0) {
+			cout << "Ошибка! Стек пуст.";
+			return 0;
+		}
+		if (first == last) { last = nullptr; } 
+		Node* del = first;
+		int ans = del->param;
+		first = first->next;
+		delete del;
+		counting--;
+		return ans;
+	}
+	void clear() {
+		int kol = count();
+		for (int i = 0; i < kol; i++) {
+			Node* del = first;
+			first = first->next;
+			delete del;
+			counting--;
+		}
+	}
+	int count(int value) {
+		int ans = 0, iter = count();
+		Node* node = first;
+		for (int i = 0; i < iter; i++) {
+			if (node->param == value) { ans++; }
+			node = node->next;
+		}
+		return ans;
+	}
+	void removeNegative() {
+		Node* node = first;
+		while (true) {
+			Node* del = node;
+			last = node;
+			if (node->next == nullptr) { break; }
+			if (node->next->param < 0) {
+				del = del->next;
+				node->next = node->next->next;
+				delete del;
+				counting--;
+			}
+			else {
+				node = node->next;
+			}
+		}
+		if (first->param < 0) {
+			node = first;
+			first = first->next;
+			delete node;
+			counting--;
+		}
+	}
+	void insertBeforeNegative() {
+		Node* node = first;
+		if (node->param < 0) {
+			Node* insert = new Node();
+			insert->param = 1;
+			insert->next = node;
+			first = insert;
+			counting++;
+		}
+		while (true) {
+			if (node->next->param < 0) {
+				Node* insert = new Node();
+				insert->param = 1;
+				insert->next = node->next;
+				node->next = insert;
+				node = node->next;
+				counting++;
+			}
+			node = node->next;
+			if (node == last) { break; }
+		}
+	}
+};
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	Queue queue;
+	instruction();
+	while (true) {
+		cout << "\nВыберите функцию: ";
+		int parametr;
+		cin >> parametr;
+		switch (parametr) {
+			case 1:
+				cout << "Введите число: ";
+				int num;
+				cin >> num;
+				queue.queue(num);
+				break;
+			case 2:
+				cout << "Удаленный элемент: " << queue.unqueue() << endl;
+				break;
+			case 3:
+				cout << "Количество элементов: " << queue.count() << endl;
+				break;
+			case 4:
+				queue.clear();
+				cout << "Очередь очищена\n";
+				break;
+			case 5:
+				cout << "Введите число: ";
+				int number;
+				cin >> number;
+				cout << "Количество: " << queue.count(number) << endl;
+				break;
+			case 6:
+				queue.removeNegative();
+				break;
+			case 7:
+				queue.insertBeforeNegative();
+				break;
+			case 8:
+				return 0;
+			default:
+				cout << "Неизвестная команда!\n";
+		}
+	}
+}
